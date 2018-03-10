f9f0a4888937bd1bd3777ccbfc8a137d9e369aee
anna <an.piskunova2013@yandex.ru>
2018-03-10 22:27:27 +0300

    print tf-idf if more than 0.01

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ package myalgoritm;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Main {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
 
     String dirPath, stopListPath;
     int totalDocuments = 0;
@@ -14,12 +18,23 @@ public class Main {
     ArrayList<String> stopWordList = new ArrayList<String>();
     TreeMap<String, Integer> wordList = new TreeMap<String, Integer>();
     TreeMap<String, TreeMap<String, Integer>> wordOccurrence = new TreeMap<String, TreeMap<String, Integer>>();
-    TreeMap<String, Integer> documentLength = new TreeMap<String, Integer>();
-    String punctuationMarks = "[^a-zA-Zа]";
+    TreeMap<String, TreeMap<String, Integer>> wordOccurrenceSmall = new TreeMap<String, TreeMap<String, Integer>>();
+    Map<String, Integer> documentLength = new HashMap<String, Integer>();
+    String punctuationMarks = "[^a-zA-Zа\\s]";
 
     public Main(String directory, String stoplist) {
         dirPath = directory;
         stopListPath = stoplist;
+    }
+
+    public double countTF(double wordsOccur, double wordsCount) {
+
+        return wordsOccur/wordsCount;
+    }
+
+    public double countIDF(double documentsCount, double documentsWithWordCount) {
+
+        return Math.log10(documentsCount/documentsWithWordCount);
     }
 
     /**
@@ -41,7 +56,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
         while((line = br.readLine()) != null) {
-            stopWordList.add(line.trim().toLowerCase());
+            try {
+                stopWordList.add(Snow.stemSnowBall(line.trim().toLowerCase()));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            //System.out.println("stop: "+stopWordList);
         }
     }
 
@@ -82,15 +110,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
         while((line = br.readLine()) != null) {
+            if (line.length()==0) {
+                continue;
+            }
+            line = line.replaceAll(punctuationMarks,"");
             String wordsInLine[] = line.split(whiteSpacePattern);
             for (i = 0; i < wordsInLine.length; i++) {
                 word = wordsInLine[i].trim().toLowerCase();
-                word = word.replaceAll(punctuationMarks,"");
 
-                if (isStopListWord(word)) {
-                    wordCounter+=1;
+                if(word.length()==0) {
                     continue;
                 }
+
+
                 try {
                     word = Snow.stemSnowBall(word);
                 } catch (ClassNotFoundException e) {
@@ -104,6 +136,11 @@ public class Main {
                 } catch (InvocationTargetException e) {
                     e.printStackTrace();
                 }
+                if (isStopListWord(word)) {
+                    wordCounter+=1;
+                    continue;
+                }
+
                 if (wordList.containsKey(word)) {
                     wordFreq = wordOccurrence.get(word);
                     if (wordFreq.containsKey(fileName)) {
@@ -149,8 +186,38 @@ public class Main {
         }
 
         for (Map.Entry<String, Integer> entry : wordList.entrySet()) {
-            System.out.println("Key: " + entry.getKey() + ". Value: " + entry.getValue());
+            //System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
         }
+
+        for (Map.Entry<String,TreeMap<String,Integer>> entry : wordOccurrence.entrySet()) {
+            double tf=0, idf=0;
+            String word = entry.getKey();
+            System.out.println("word: " + word);
+            for (Map.Entry<String,Integer>entryEntry:
+                 entry.getValue().entrySet()) {
+
+                Integer count = entryEntry.getValue();
+                String file = entryEntry.getKey();
+                System.out.println("file: " + file + " count: " + count);
+                tf = this.countTF((double)count,(double)documentLength.get(file));
+                idf = this.countIDF((double)totalDocuments,(double)entry.getValue().size());
+            }
+
+            System.out.println("tf: "+tf);
+            System.out.println("idf: "+tf);
+            if (tf*idf>0.01){
+                System.out.println(ANSI_RED + "tf-idf: " + tf*idf+ANSI_RESET);
+            }
+            System.out.println("countDocum: "+ entry.getValue().size());
+        }
+
+//        for (Map.Entry<String,TreeMap<String,Integer>> entry : wordOccurrence.entrySet()) {
+//            System.out.println("word: " + entry.getKey());
+//            for (Map.Entry<String,Integer>entryEntry:
+//                    entry.getValue().entrySet()) {
+//                System.out.println("file: " + entryEntry.getKey() + " count: " + entryEntry.getValue());
+//            }
+//        }
 
     }
 
